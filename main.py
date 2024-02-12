eea13e0d44a5922cd19517b1cd672eeffd8b46e9
Gnanadeep-Settykara <gsettyka@asu.edu>
2024-02-12 16:22:27 -0700

    Added sqlite3 database

--- main.py
+++ main.py
@@ -3,21 +3,17 @@ import sys
 import sqlite3
 from matplotlib import cm
 from PySide6.QtCore import Qt, Slot
-from PySide6.QtGui import QAction , QPainter, QBrush, QColor
-from PySide6.QtWidgets import (QApplication, QHeaderView, QHBoxLayout, QLabel, QLineEdit, 
-                               QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout, 
-                               QWidget, QMessageBox)
+from PySide6.QtGui import QAction, QPainter, QBrush, QColor
+from PySide6.QtWidgets import (QApplication, QHeaderView, QHBoxLayout, QLabel, QLineEdit,
+                               QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout,
+                               QWidget, QMessageBox, QFormLayout)
 from PySide6.QtCharts import QChartView, QPieSeries, QChart
-
 
 
 class Widget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.items = 0
-        #Dummy Data
-        self._data = {"Rent": 1600, "Grocery": 300, "Water": 50, "Coffee": 45, "Phone": 30, "Internet": 60}
-
 
         # Database connection
         self.connect_to_sqlite()
@@ -33,18 +29,20 @@ class Widget(QWidget):
         self.chart_view.setRenderHint(QPainter.Antialiasing)
 
         colormap = cm.get_cmap("tab20c", 40)  # You can choose a different colormap
-        self.colors = [QColor.fromRgbF(*colormap(i)[:3]) for i in range(0,40,2)]
+        self.colors = [QColor.fromRgbF(*colormap(i)[:3]) for i in range(0, 40, 2)]
 
         # Right Widget
         self.description = QLineEdit()
         self.price = QLineEdit()
         self.add = QPushButton("Add")
+        self.delete = QPushButton("Delete")
         self.clear = QPushButton("Clear")
         self.quit = QPushButton("Quit")
         self.plot = QPushButton("Plot")
 
-        # Disabling 'Add' button
+        # Disabling 'Add' button initially
         self.add.setEnabled(False)
+        self.delete.setEnabled(False)
 
         self.right = QVBoxLayout()
         self.right.addWidget(QLabel("Description"))
@@ -52,6 +50,7 @@ class Widget(QWidget):
         self.right.addWidget(QLabel("Price"))
         self.right.addWidget(self.price)
         self.right.addWidget(self.add)
+        self.right.addWidget(self.delete)
         self.right.addWidget(self.plot)
         self.right.addWidget(self.chart_view)
         self.right.addWidget(self.clear)
@@ -60,7 +59,7 @@ class Widget(QWidget):
         # QWidget Layout
         self.layout = QHBoxLayout()
 
-        #self.table_view.setSizePolicy(size)
+        # self.table_view.setSizePolicy(size)
         self.layout.addWidget(self.table)
         self.layout.addLayout(self.right)
 
@@ -69,14 +68,19 @@ class Widget(QWidget):
 
         # Signals and Slots
         self.add.clicked.connect(self.add_element)
+        self.delete.clicked.connect(self.delete_element)
         self.quit.clicked.connect(self.quit_application)
         self.plot.clicked.connect(self.plot_data)
         self.clear.clicked.connect(self.clear_table)
         self.description.textChanged[str].connect(self.check_disable)
         self.price.textChanged[str].connect(self.check_disable)
 
+        # Table selection change signal
+        self.table.itemSelectionChanged.connect(self.enable_disable_buttons)
+
         # Fill example data
         self.fill_table()
+        # Fetch data from the database and update the table
         self.fetch_data_from_database()
 
     def connect_to_sqlite(self):
@@ -93,17 +97,31 @@ class Widget(QWidget):
         """)
         self.connection.commit()
 
+        # Add dummy data if the table is empty
+        self.cursor.execute("SELECT COUNT(*) FROM expenses")
+        count = self.cursor.fetchone()[0]
+        if count == 0:
+            dummy_data = [
+                ("Rent", 1600),
+                ("Grocery", 300),
+                ("Water", 50),
+                ("Coffee", 45),
+                ("Phone", 30),
+                ("Internet", 60)
+            ]
+            self.cursor.executemany("INSERT INTO expenses (description, price) VALUES (?, ?)", dummy_data)
+            self.connection.commit()
+
     def fetch_data_from_database(self):
         self.cursor.execute("SELECT * FROM expenses")
         rows = self.cursor.fetchall()
-        for row in rows:
-            description_item = QTableWidgetItem(row[1])
-            price_item = QTableWidgetItem(f"{row[2]:.2f}")
-            price_item.setTextAlignment(Qt.AlignRight)
-            self.table.insertRow(self.items)
-            self.table.setItem(self.items, 0, description_item)
-            self.table.setItem(self.items, 1, price_item)
-            self.items += 1
+        return rows
+
+    def update_table(self):
+        self.table.setRowCount(0)
+        self.items = 0
+        self.fetch_data_from_database()
+        self.fill_table()
 
     @Slot()
     def add_element(self):
@@ -115,44 +133,51 @@ class Widget(QWidget):
             self.cursor.execute("INSERT INTO expenses (description, price) VALUES (?, ?)", (des, price_value))
             self.connection.commit()
 
-            price_item = QTableWidgetItem(f"{price_value:.2f}")
-            price_item.setTextAlignment(Qt.AlignRight)
-
-            self.table.insertRow(self.items)
-            description_item = QTableWidgetItem(des)
-
-            self.table.setItem(self.items, 0, description_item)
-            self.table.setItem(self.items, 1, price_item)
+            self.update_table()
 
             self.description.setText("")
             self.price.setText("")
-
-            self.items += 1
         except ValueError as e:
             # Display an error message box with the ValueError information
             error_message = f"Invalid input: {price}\nPlease enter a valid Price!"
             QMessageBox.critical(self, "Error", error_message)
 
     @Slot()
+    def delete_element(self):
+        selected_row = self.table.currentRow()
+        if selected_row != -1:
+            expense_id = self._get_expense_id(selected_row)
+            self.cursor.execute("DELETE FROM expenses WHERE id=?", (expense_id,))
+            self.connection.commit()
+
+            self.update_table()
+
+            self.description.setText("")
+            self.price.setText("")
+
+    def _get_expense_id(self, row_index):
+        return self.table.item(row_index, 0).data(Qt.UserRole)
+
+    @Slot()
     def clear_table(self):
         self.cursor.execute("DELETE FROM expenses")
         self.connection.commit()
-        self.table.setRowCount(0)
-        self.items = 0
-
+        self.update_table()
 
     @Slot()
     def check_disable(self, x):
         if not self.description.text() or not self.price.text():
             self.add.setEnabled(False)
+            self.delete.setEnabled(False)
         else:
             self.add.setEnabled(True)
+            self.delete.setEnabled(True)
 
     @Slot()
     def plot_data(self):
         # Get table information
         series = QPieSeries()
-        
+
         for i in range(self.table.rowCount()):
             text = self.table.item(i, 0).text()
             number = float(self.table.item(i, 1).text())
@@ -171,20 +196,27 @@ class Widget(QWidget):
         QApplication.quit()
 
     def fill_table(self, data=None):
-        data = self._data if not data else data
-        for desc, price in data.items():
-            description_item = QTableWidgetItem(desc)
-            price_item = QTableWidgetItem(f"{price:.2f}")
+        data = self.fetch_data_from_database() if not data else data
+        for row in data:
+            description_item = QTableWidgetItem(row[1])
+
+            price_item = QTableWidgetItem(f"{row[2]:.2f}")
+
             price_item.setTextAlignment(Qt.AlignRight)
+            description_item.setData(Qt.UserRole, row[0])
+
             self.table.insertRow(self.items)
             self.table.setItem(self.items, 0, description_item)
             self.table.setItem(self.items, 1, price_item)
             self.items += 1
 
     @Slot()
-    def clear_table(self):
-        self.table.setRowCount(0)
-        self.items = 0
+    def enable_disable_buttons(self):
+        selected_row = self.table.currentRow()
+        if selected_row != -1:
+            self.delete.setEnabled(True)
+        else:
+            self.delete.setEnabled(False)
 
 
 class MainWindow(QMainWindow):
